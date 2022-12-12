f06476c0f045c92b47a272560a761061d1e714dc
Mikhail.kovalevsckij@gmail.com <Mikhail.kovalevsckij@gmail.com>
2022-12-12 14:04:31 +0800

    Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача №15
+
+Console.WriteLine("Введите день недели");
+uint day = Convert.ToUInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("working");
+        break;
+    case 2:
+        Console.WriteLine("working");
+        break;
+    case 3:
+        Console.WriteLine("working");
+        break;
+    case 4:
+        Console.WriteLine("working");
+        break;
+    case 5:
+        Console.WriteLine("working");
+        break;
+    case 6:
+        Console.WriteLine("day off");
+        break;
+    case 7:
+        Console.WriteLine("day off");
+        break;
+    default:
+        Console.WriteLine("No day");
+        break;
+}
